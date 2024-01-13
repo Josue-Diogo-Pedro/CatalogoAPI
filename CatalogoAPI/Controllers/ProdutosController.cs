b9571951edbc1d102e35317d23684d0c2dcf9911
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-01-13 08:59:39 -0800

    Adding ProdutosController

---
+++ CatalogoAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using CatalogoAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+	public ProdutosController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+
+}
