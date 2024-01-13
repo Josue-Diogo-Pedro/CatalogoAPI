126f8e6189e110faaf890ba75931fe8d768cff2a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-01-13 09:32:52 -0800

    Add method action to delete especify product

--- CatalogoAPI/Controllers/ProdutosController.cs
+++ CatalogoAPI/Controllers/ProdutosController.cs
@@ -62,4 +62,17 @@ public class ProdutosController : ControllerBase
 
 		return Ok(produto);
 	}
+
+	[HttpDelete("{id:int}")]
+	public ActionResult Delete(int id)
+	{
+		var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+		if (produto is null)
+			return NotFound("Produto não encontrado...");
+
+		_context.Produtos.Remove(produto);
+		_context.SaveChanges();
+
+		return Ok(produto);
+	}
 }
