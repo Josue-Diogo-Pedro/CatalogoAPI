f4e7ee4901d5cc38624c9405b99d6711c00bbc35
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-01-12 19:05:07 -0800

    Add ConnectionString and register services in program

---
+++ CatalogoAPI/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
