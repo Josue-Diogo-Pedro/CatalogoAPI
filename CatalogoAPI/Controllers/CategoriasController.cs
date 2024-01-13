70073218bb301276338d134e443bf2e7a1bd4709
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-01-13 09:53:56 -0800

    Building CategoriaController

---
+++ CatalogoAPI/Controllers/CategoriasController.cs
@@ -0,0 +1,32 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+	public CategoriasController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet("produtos")]
+	public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+	{
+		return _context.Categorias.Include(p => p.Produtos).ToList();
+	}
+
+	[HttpGet]
+	public ActionResult<IEnumerable<Categoria>> Get()
+	{
+		return _context.Categorias.ToList();
+	}
+
+
+}
