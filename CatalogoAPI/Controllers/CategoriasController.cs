425144cb685ac68a54f306f88672d8d3dd65c08a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-01-13 10:00:51 -0800

    Finish to build CategoriaController

--- CatalogoAPI/Controllers/CategoriasController.cs
+++ CatalogoAPI/Controllers/CategoriasController.cs
@@ -25,8 +25,58 @@ public class CategoriasController : ControllerBase
 	[HttpGet]
 	public ActionResult<IEnumerable<Categoria>> Get()
 	{
-		return _context.Categorias.ToList();
+		var categorias = _context.Categorias.ToList();
+		if (categorias is null)
+			return NotFound("Categorias não encontradas");
+
+		return categorias;
 	}
 
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> GetById(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+        if (categoria is null)
+            return NotFound("Produto não encontrado");
 
+        return categoria;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Categoria categoria)
+    {
+        if (categoria is null)
+            return BadRequest();
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterCategoria",
+            new { id = categoria.CategoriaId, categoria });
+    }
+
+    [HttpPut]
+    public ActionResult Put(int id, Categoria categoria)
+    {
+        if (id != categoria.CategoriaId)
+            return BadRequest();
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(categoria);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+        if (categoria is null)
+            return NotFound("Produto não encontrado...");
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        return Ok(categoria);
+    }
 }
