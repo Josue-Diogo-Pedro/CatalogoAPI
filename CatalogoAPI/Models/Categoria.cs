ebc7a953c58ca1fe4af7dcb230032c98ce62248f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-01-12 18:55:31 -0800

    Creating models of project. Categoria and Produto

---
+++ CatalogoAPI/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CatalogoAPI.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    public int CategoriaId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
+}
---
+++ CatalogoAPI/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogoAPI.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+}
