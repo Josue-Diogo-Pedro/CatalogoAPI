247444e66a416fb0d2c93650257750368102042f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-13 01:07:38 -0800

    Finish to implement UnitOfWork

--- CatalogoAPI/Repository/UnitOfWork.cs
+++ CatalogoAPI/Repository/UnitOfWork.cs
@@ -1,6 +1,33 @@
-﻿namespace CatalogoAPI.Repository;
+﻿using CatalogoAPI.Context;
+using Microsoft.EntityFrameworkCore;
 
-public class UnitOfWork
+namespace CatalogoAPI.Repository;
+
+public class UnitOfWork : IUnitOfWork
 {
+    private ProdutoRepository _produtoRepository;
+    private CategoriaRepository _categoriaRepository;
+    private AppDbContext _context;
 
+    public UnitOfWork(AppDbContext context) => _context = context;
+
+    public IProdutoRepository ProdutoRepository 
+    {
+        get
+        {
+            return _produtoRepository ?? new ProdutoRepository(_context);
+        }
+    }
+
+    public ICategoriaRepository CategoriaRepository 
+    {
+        get
+        {
+            return _categoriaRepository ?? new CategoriaRepository(_context);
+        }
+    }
+
+    public void Commit() => _context.SaveChanges();
+
+    public void Dispose() => _context.Dispose();
 }
