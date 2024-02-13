90c3de7154b17fff22c70fe5f1d8e2446775786f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-13 07:53:36 -0800

    Finish to implement CategoriaRepository

--- CatalogoAPI/Repository/CategoriaRepository.cs
+++ CatalogoAPI/Repository/CategoriaRepository.cs
@@ -1,7 +1,15 @@
-﻿using CatalogoAPI.Models;
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogoAPI.Repository;
 
-public class CategoriaRepository : Repository<Categoria>
+public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
 {
+    public CategoriaRepository(AppDbContext context) : base(context) { }
+
+    public IEnumerable<Categoria> GetCategoriasProdutos()
+    {
+        return Get().Include(p => p.Produtos);
+    }
 }
