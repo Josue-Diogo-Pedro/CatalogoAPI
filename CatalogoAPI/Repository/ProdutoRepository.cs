a046f96157159a609e4a37eeb1ed409c224b6d0a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-12 22:46:24 -0800

    Finish to implement ProdutoRepository

---
+++ CatalogoAPI/Repository/ProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+
+namespace CatalogoAPI.Repository;
+
+public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+{
+    public ProdutoRepository(AppDbContext context) : base(context) { }
+
+    public IEnumerable<Produto> GetProdutosPorPreco() => Get().OrderBy(p => p.Preco).ToList();
+}
