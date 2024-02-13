8a9c4aafb522fb23e2a232d470d8603253a9bfcf
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-12 22:40:53 -0800

    In update, using entryState

--- CatalogoAPI/Repository/Repository.cs
+++ CatalogoAPI/Repository/Repository.cs
@@ -16,7 +16,11 @@ public class Repository<T> : IRepository<T> where T : class
 
     public void Add(T entity) => _context.Set<T>().Add(entity);
 
-    public void Update(T entity) => _context.Set<T>().Update(entity);
+    public void Update(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        _context.Set<T>().Update(entity);
+    }
 
     public void Delete(T entity) => _context.Set<T>().Remove(entity);
 }
