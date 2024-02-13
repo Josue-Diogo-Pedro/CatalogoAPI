20dba02b5f68468037e709a959984101906db19d
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-12 22:26:10 -0800

    Finish to implement IRepository of T

--- CatalogoAPI/Repository/IRepository.cs
+++ CatalogoAPI/Repository/IRepository.cs
@@ -6,5 +6,7 @@ public interface IRepository<T>
 {
     IQueryable<T> Get();
     T GetById(Expression<Func<T, bool>> predicate);
-
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
 }
