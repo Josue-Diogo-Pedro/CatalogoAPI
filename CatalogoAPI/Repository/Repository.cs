c65770a3bec81e2aed45ad2220110c8ab6371cfe
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-12 22:28:44 -0800

    Implementing Repository of T

---
+++ CatalogoAPI/Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using CatalogoAPI.Context;
+using System.Linq.Expressions;
+
+namespace CatalogoAPI.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected AppDbContext _context;
+
+    public Repository()
+    {
+
+    }
+
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetById(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
