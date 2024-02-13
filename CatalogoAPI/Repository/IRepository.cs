4740607f7891b1b512d41b38a5ade20bea0a2b3b
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-12 22:25:07 -0800

    Creating IRepository of T

---
+++ CatalogoAPI/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace CatalogoAPI.Repository;
+
+public interface IRepository<T>
+{
+    IQueryable<T> Get();
+    T GetById(Expression<Func<T, bool>> predicate);
+
+}
