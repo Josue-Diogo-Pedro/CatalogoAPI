6cf810e302c7e18dda46c4bab2d08f9074527d1d
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-17 14:21:10 -0800

    Fnish to create ProdutoDTO

--- CatalogoAPI/DTOs/ProdutoDTO.cs
+++ CatalogoAPI/DTOs/ProdutoDTO.cs
@@ -2,5 +2,10 @@
 
 public class ProdutoDTO
 {
-
+    public int ProdutoId { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+    public int CategoriaId { get; set; }
 }
