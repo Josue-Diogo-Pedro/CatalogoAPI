3655caac01c537cfa7dfe9346fbf86b0e6116f02
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-17 14:25:01 -0800

    Finish to implement MappingProfile

--- CatalogoAPI/DTOs/Mappings/MappingProfile.cs
+++ CatalogoAPI/DTOs/Mappings/MappingProfile.cs
@@ -1,5 +1,13 @@
-﻿namespace CatalogoAPI.DTOs.Mappings;
+﻿using AutoMapper;
+using CatalogoAPI.Models;
 
-public class MappingProfile
+namespace CatalogoAPI.DTOs.Mappings;
+
+public class MappingProfile : Profile
 {
+	public MappingProfile()
+	{
+		CreateMap<Produto, ProdutoDTO>().ReverseMap();
+		CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+	}
 }
