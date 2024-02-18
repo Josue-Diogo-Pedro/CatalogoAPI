0f6fc2ad57e177771ee8b1ce2a5b67c73f473c66
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-18 05:26:07 -0800

    Create a method to return with pagination in IProdutoRepository

--- CatalogoAPI/Repository/IProdutoRepository.cs
+++ CatalogoAPI/Repository/IProdutoRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CatalogoAPI.Models;
+using CatalogoAPI.Pagination;
 
 namespace CatalogoAPI.Repository;
 
 public interface IProdutoRepository : IRepository<Produto>
 {
+    IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParameters);
     IEnumerable<Produto> GetProdutosPorPreco();
 }
