dd5b760355baa28960679175350420407d6a3a13
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-18 05:28:31 -0800

    Finish to implement in class Repository

--- CatalogoAPI/Repository/ProdutoRepository.cs
+++ CatalogoAPI/Repository/ProdutoRepository.cs
@@ -1,11 +1,21 @@
 ﻿using CatalogoAPI.Context;
 using CatalogoAPI.Models;
+using CatalogoAPI.Pagination;
 
 namespace CatalogoAPI.Repository;
 
 public class ProdutoRepository : Repository<Produto>, IProdutoRepository
 {
     public ProdutoRepository(AppDbContext context) : base(context) { }
+
+    public IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParameters)
+    {
+        return Get()
+            .OrderBy(nome => nome.Nome)
+            .Skip((produtosParameters.PageNumber - 1) * produtosParameters.PageSize)
+            .Take(produtosParameters.PageSize)
+            .ToList();
+    }
 
     public IEnumerable<Produto> GetProdutosPorPreco() => Get().OrderBy(p => p.Preco).ToList();
 }
