37044292f188728034aed155726b715a47e1974e
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-18 05:24:17 -0800

    Finish to implement ProdutosParamenters

--- CatalogoAPI/Pagination/ProdutosParameters.cs
+++ CatalogoAPI/Pagination/ProdutosParameters.cs
@@ -2,5 +2,15 @@
 
 public class ProdutosParameters
 {
-
+    const int maxPageSize = 3;
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 2;
+    public int PageSize
+    {
+        get { return _pageSize; }
+        set
+        {
+            _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
 }
