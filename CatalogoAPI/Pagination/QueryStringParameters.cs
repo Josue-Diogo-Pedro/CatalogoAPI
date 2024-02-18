605b3d81d06fab39f6aaaf3ec5671f4803f37ae4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-18 07:25:00 -0800

    Creating QueryStringParameters to use Parameters on geral form

---
+++ CatalogoAPI/Pagination/QueryStringParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace CatalogoAPI.Pagination;
+
+public abstract class QueryStringParameters
+{
+    const int maxPageSize = 3;
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 2;
+    public int PageSize
+    {
+        get { return _pageSize; }
+        set
+        {
+            _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
+}
