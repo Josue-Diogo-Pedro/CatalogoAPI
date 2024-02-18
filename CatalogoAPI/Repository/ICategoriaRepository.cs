4cc437d9bb0adc27191cf1b245d5d5a70c8f1b50
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-18 07:27:41 -0800

    Create method in ICategoriaRepository

--- CatalogoAPI/Repository/ICategoriaRepository.cs
+++ CatalogoAPI/Repository/ICategoriaRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CatalogoAPI.Models;
+using CatalogoAPI.Pagination;
 
 namespace CatalogoAPI.Repository;
 
 public interface ICategoriaRepository : IRepository<Categoria>
 {
+    PagedList<Categoria> GetCategorias(CategoriasParameters categoriasParameters);
     IEnumerable<Categoria> GetCategoriasProdutos();
 }
