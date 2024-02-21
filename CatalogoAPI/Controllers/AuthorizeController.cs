1528ff7ee96cb0f1a655943c00ceb12fe020d77b
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-21 02:13:13 -0800

    Finish to implement register user

--- CatalogoAPI/Controllers/AuthorizeController.cs
+++ CatalogoAPI/Controllers/AuthorizeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CatalogoAPI.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogoAPI.Controllers;
 
@@ -6,5 +8,35 @@ namespace CatalogoAPI.Controllers;
 [ApiController]
 public class AuthorizeController : ControllerBase
 {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
 
+    public AuthorizeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public string Get() => "AuthorizeController :: Acessado em : " + DateTime.Now.ToLongDateString();
+
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody]UsuarioDTO model)
+    {
+        if(!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(error => error.Errors));
+
+        IdentityUser user = new()
+        {
+            UserName = model.Email,
+            Email = model.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded) return BadRequest(result.Errors);
+
+        await _signInManager.SignInAsync(user, false);
+        return Ok();
+    }
 }
