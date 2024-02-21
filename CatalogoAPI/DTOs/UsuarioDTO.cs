80e42a1833c5f97ab163a551f568c01448808528
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-21 01:57:23 -0800

    Add class UsuarioDTO that will transfer data to IdentityUser

---
+++ CatalogoAPI/DTOs/UsuarioDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CatalogoAPI.DTOs;
+
+public class UsuarioDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
