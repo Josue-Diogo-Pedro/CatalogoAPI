750b90e6ee39349f944b7d08a7568d0fa7742b10
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-21 03:11:57 -0800

    Model where contain attributes of token

---
+++ CatalogoAPI/DTOs/UsuarioToken.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogoAPI.DTOs;
+
+public class UsuarioToken
+{
+    public bool Authenticated { get; set; }
+    public DateTime Expiration { get; set; }
+    public string Token { get; set; }
+    public string Message { get; set; }
+}
