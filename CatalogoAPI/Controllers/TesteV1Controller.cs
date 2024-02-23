a4db6cb2c8411d65ff1de970b70024b0e3debdb7
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-23 15:24:53 -0800

    Creating the controllers tests

---
+++ CatalogoAPI/Controllers/TesteV1Controller.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TesteV1Controller : ControllerBase
+{
+}
---
+++ CatalogoAPI/Controllers/Testev2Controller.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Testev2Controller : ControllerBase
+{
+}
