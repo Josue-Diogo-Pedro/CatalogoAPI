46637bc966bf2a5794f74cd89970e71c20775968
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-25 07:32:52 -0800

    Part of implemention of CategoriasUnitTestController

--- CatalogoApixUnitTests/CategoriasUnitTestController.cs
+++ CatalogoApixUnitTests/CategoriasUnitTestController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CatalogoAPI.Context;
+using CatalogoAPI.DTOs.Mappings;
 using CatalogoAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,4 +14,23 @@ public class CategoriasUnitTestController
     public static DbContextOptions<AppDbContext> dbContextOptions;
 
     private static string connectString = "Data Source=DESKTOP-DSNQA59\\SQLEXPRESS02;Initial Catalog=CatalogoAPI;Integrated Security=True; Trust Server Certificate=true;MultipleActiveResultSets=true";
+
+    //Construtor estático é o primeiro a iniciar quando uma classe é instanciada
+    static CategoriasUnitTestController()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(connectString)
+            .Options;
+    }
+
+    public CategoriasUnitTestController()
+    {
+        var config = new MapperConfiguration(cfg =>
+            cfg.AddProfile(new MappingProfile()));
+        mapper = config.CreateMapper();
+
+        var context = new AppDbContext(dbContextOptions);
+
+        var repository = new UnitOfWork(context);
+    }
 }
