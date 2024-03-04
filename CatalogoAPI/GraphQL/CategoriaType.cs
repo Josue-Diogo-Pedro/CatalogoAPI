b21f2eb16609c2e26e23a8091c2cef8347d7c146
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-04 13:54:13 -0800

    Finish to write CategoriaType

--- CatalogoAPI/GraphQL/CategoriaType.cs
+++ CatalogoAPI/GraphQL/CategoriaType.cs
@@ -1,5 +1,18 @@
-﻿namespace CatalogoAPI.GraphQL;
+﻿using CatalogoAPI.Models;
+using GraphQL.Types;
 
-public class CategoriaType
+namespace CatalogoAPI.GraphQL;
+
+//Estamos definindo qual entidade será mapeada para o nosso Type
+public class CategoriaType : ObjectGraphType<Categoria>
 {
+	public CategoriaType()
+	{
+		//Campos Type
+		Field(x => x.CategoriaId);
+		Field(x => x.Nome);
+		Field(x => x.ImagemUrl);
+
+		Field<ListGraphType<CategoriaType>>("categorias");
+	}
 }
