f98cb3faac7368df3618b3b34e5fdc4b50dc34a4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-04 14:05:26 -0800

    First part of code

--- CatalogoAPI/GraphQL/CategoriaQuery.cs
+++ CatalogoAPI/GraphQL/CategoriaQuery.cs
@@ -1,6 +1,25 @@
-﻿namespace CatalogoAPI.GraphQL
+﻿using CatalogoAPI.Repository;
+using GraphQL;
+using GraphQL.Types;
+
+namespace CatalogoAPI.GraphQL;
+
+//Mapeamos os campos para uma dada consulta
+//para uma chamada do repositorio (CategoriasRepository)
+public class CategoriaQuery : ObjectGraphType
 {
-    public class CategoriaQuery
-    {
-    }
+	//Recebe a instância do nosso UnitOfWork que contém
+	//as instâncias dos repositorios
+	public CategoriaQuery(IUnitOfWork _context)
+	{
+		//Nosso método vai retornar um objecto categoria
+		Field<CategoriaType>("categoria")
+			.Arguments(new QueryArguments(
+				new QueryArgument<IntGraphType>() { Name = "id" }))
+			.ResolveAsync(async context =>
+			{
+				var id = context.GetArgument<int>("id");
+				return await _context.CategoriaRepository.GetById(c => c.CategoriaId == id);
+			});
+	}
 }
