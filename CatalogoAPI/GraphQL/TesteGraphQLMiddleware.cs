df9ef5829e3a188def224884c6dee6be38686cca
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-04 14:18:31 -0800

    Finish to crete properties

--- CatalogoAPI/GraphQL/TesteGraphQLMiddleware.cs
+++ CatalogoAPI/GraphQL/TesteGraphQLMiddleware.cs
@@ -1,5 +1,16 @@
-﻿namespace CatalogoAPI.GraphQL;
+﻿using CatalogoAPI.Repository;
 
+namespace CatalogoAPI.GraphQL;
+
+//É incluido no pipeline do request para processar
+//a requisição http usando a instância do nosso repositório
 public class TesteGraphQLMiddleware
 {
+    //instancia para processar o request http
+    private readonly RequestDelegate _next;
+
+    //Instancia do UnitOfWork
+    private readonly UnitOfWork _context;
+
+
 }
